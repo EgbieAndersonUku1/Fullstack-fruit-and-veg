0eabeaf174242b7afb9be84c9c4a746b45d4aac3
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-08-15 18:36:11 +0100

    Added an `app` inside main since it seems Vercel is looking for it.

--- src/manage.py
+++ src/manage.py
@@ -3,10 +3,13 @@
 import os
 import sys
 
+from django.core.wsgi import get_wsgi_application
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fruit_and_veg.settings')
+    application = get_wsgi_application()
+    app = application  # Add this alias for Vercel
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,4 +22,6 @@ def main():
 
 
 if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
     main()
