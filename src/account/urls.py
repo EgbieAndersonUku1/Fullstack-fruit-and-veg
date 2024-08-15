078484d3d7e03c56c3689f785462a9e51e41a283
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-08-16 00:05:57 +0100

    feat: Convert static file paths to Django static tags and create reusable partials
    - Converted image paths in various templates to use Django's `{% static %}` tag.
    - Created partials for common components:
      - Added `logo_link.html` for the site logo linking to the homepage.
      - Added `nav-icons.html` for navigation icons.
      - Added `nav-menu.html` for the navigation menu.
    - Updated the following templates to include the new partials:
      - `account.html`
      - `financial-management.html`
      - `invoice.html`
      - `orders.html`
      - `view-item.html`
      - `index.html`
      - `base_nav.html`
    - Modified corresponding URLs and views to accommodate these changes.
    
    These updates ensure that when one navigates to these files, it works seamlessly with the new static file paths and partials in place.

--- src/account/urls.py
+++ src/account/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 urlpatterns = [
    path("account", view=views.account, name="account"),
-   path("account/product-management/", view=views.product_management, name="product-management"),
+   path("account/product-management/overview/", view=views.product_management, name="product-management"),
    path("account/product-management/add-new-product/basic-description/", view=views.add_basic_description, name="add_new_product"),
-   path("account/product-management/add-new-product/detailed-description/", view=views.add_detailed_description, name="add_detailed_description")
+   path("account/product-management/add-new-product/detailed-description/", view=views.add_detailed_description, name="add_detailed_description"),
+   path("account/orders/order/", view=views.orders, name="orders"),
+   path("account/orders/view-item/<int:id>", views.view_item, name="view-item"),
+   path("account/financial-management/overview/", view=views.financial_management, name="financial_management"),
+   path("account/orders/order/invoice/<int:item_id>/", view=views.invoice, name="invoice"),
    
 ]
--- src/account/views.py
+++ src/account/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 
-
 def account(request):
     return render(request, "account/account/account.html")
 
@@ -18,3 +17,20 @@ def add_basic_description(request):
 
 def add_detailed_description(request):
     return render(request, "account/product-management/add-new-product/detailed-descripton-specs.html")
+
+
+def orders(request):
+    return render(request, "account/orders/orders.html")
+
+
+def view_item(request, id):
+    return render(request, "account/orders/view-item.html")
+
+
+def financial_management(request):
+    return render(request, "account/financial-management/financial-management.html")
+
+
+
+def invoice(request, item_id):
+    return render(request, "account/orders/invoice.html" )
