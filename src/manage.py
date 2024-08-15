086f2e6ec8d3f275b33886664620772f974a64f6
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-08-15 18:41:34 +0100

    Modified the `manage.py` to include an `app` for Vercel

--- src/manage.py
+++ src/manage.py
@@ -20,8 +20,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
     main()
+
