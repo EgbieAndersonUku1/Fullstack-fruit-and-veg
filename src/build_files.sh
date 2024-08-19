71c180122131f1a6e2eda2495c88491c0c530051
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-08-20 00:29:31 +0100

    Update build.sh for Vercel compatibility

--- src/build_files.sh
+++ src/build_files.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y python3 python3-pip
 
 # get the latest pip
-# python3.12 -m pip install --upgrade pip
+python3.12 -m pip install --upgrade pip
 
 # Set up the Python environment
 pip3 install -r requirements.txt
