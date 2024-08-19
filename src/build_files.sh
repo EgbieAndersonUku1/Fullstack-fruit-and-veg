50e63ba301e6fb0a281edbdee6f6f8ac4ec3702b
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-08-19 23:36:37 +0100

    Remove the apt-get from build.sh because Vercel is having trouble

--- src/build_files.sh
+++ src/build_files.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# Install Python
-apt-get update
-apt-get install -y python3 python3-pip
+# Install dependencies
+pip install -r requirements.txt
 
-# get the latest pip
-# python3.12 -m pip install --upgrade pip
-
-# Set up the Python environment
-pip3 install -r requirements.txt
-
-# Run your build commands
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py collectstatic --noinput
-
+# Run Django management commands
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
